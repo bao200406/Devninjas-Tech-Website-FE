fc1620126f6bf9f279d66966dca964eac9e0cbd0
baolc-nexle <baolc@nexlesoft.com>
2026-06-12 16:18:14 +0700

    update logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,14 @@ export default function Header() {
       {/* Tầng trên: Logo, Tìm kiếm, Icons */}
       <div className="container mx-auto px-4 py-4 flex items-center justify-between gap-4 ">
         {/* Logo */}
-        <div className="flex items-center gap-2 shrink-0">
-          <div className="w-8 h-8 md:w-10 md:h-10 bg-blue-600 rounded-lg flex items-center justify-center">
-            <div className="w-4 h-4 md:w-6 md:h-6 border-2 border-white rounded-full"></div>
+       <div className="flex items-center gap-3 mb-6">
+          <div className="w-10 h-10 bg-blue-700 rounded-md flex items-center justify-center text-white font-bold">
+            A
           </div>
-          <span className="text-lg md:text-xl font-bold text-blue-900 tracking-tight">Azure Logic</span>
+
+          <h2 className="text-2xl font-bold text-blue-700">
+            Azure Logic
+          </h2>
         </div>
 
         {/* Search Bar - Ẩn trên Mobile, hiện trên Tablet/Desktop */}
