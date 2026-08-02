21e9d8930cf61c7c658ce4c7ae905f31a5c97ac2
baolc-nexle <baolc@nexlesoft.com>
2026-08-02 22:11:05 +0700

    update code

--- src/app/admin/products/page.jsx
+++
@@ -1,9 +0,0 @@
-import TableProduct from "../../../components/products/TableProducts";
-import { getAllProducts } from "../../../services/productService";
-export default async function ProductPage() {
-  return (
-    <>
-      <TableProduct />
-    </>
-  );
-}
