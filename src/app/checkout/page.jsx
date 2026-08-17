efe72a486b549fc35b525c62c9191c2702d8b905
bao200406 <baobi5724@gmail.com>
2026-08-17 17:01:54 +0700

    update code

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { getAddresses, createAddress } from "../../services/addressService";
 import { fetchAddressData } from "../api/addressAPI";
 import { getMe } from "../../services/authService";
